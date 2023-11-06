3d1cc1e58e1ffb2253ae3e616224ed2bc8f4633c
Debasish2017331091 <debasish1998khulna@gmail.com>
2023-11-06 22:35:04 +0600

    almost done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <>
-    <div style={{backgroundColor:"#ffffff", borderRadius:'10px', marginTop:'20p'}}>
+    <div style={{backgroundColor:"#ffffff", borderRadius:'10px', marginTop:'0px'}}>
      <Home></Home>
     </div>
     </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
-// import * as images from '../images';
-// import image from '../asstets/images/image1.jpg';
-// const images = await import('../images');
 import '../components/home.css';
 
 
 export default function Home() {
     const images = {
-        image1: require('../asstets/images/image1.jpg'),
-        image2: require('../asstets/images/image1.jpg'),
-        image3: require('../asstets/images/image1.jpg'),
-        image4: require('../asstets/images/image1.jpg'),
-        image5: require('../asstets/images/image1.jpg'),
-        image6: require('../asstets/images/image1.jpg'),
-        image7: require('../asstets/images/image1.jpg'),
-        image8: require('../asstets/images/image1.jpg'),
-        image9: require('../asstets/images/image1.jpg'),
-        image10: require('../asstets/images/image1.jpg'),
-        image11: require('../asstets/images/image1.jpg'),
-        image12: require('../asstets/images/image1.jpg'),
+      image1: require('../asstets/images/image-1.webp'),
+      image2: require('../asstets/images/image-2.webp'),
+      image3: require('../asstets/images/image-3.webp'),
+      image4: require('../asstets/images/image-4.webp'),
+      image5: require('../asstets/images/image-5.webp'),
+      image6: require('../asstets/images/image-6.webp'),
+      image7: require('../asstets/images/image-7.webp'),
+      image8: require('../asstets/images/image-8.webp'),
+      image9: require('../asstets/images/image-9.webp'),
+      image10: require('../asstets/images/image-10.jpeg'),
+      image11: require('../asstets/images/image-11.jpeg'),
     }
 
     const containerStyle = {
@@ -38,23 +34,25 @@ export default function Home() {
    
   return (
     <>
-        <div style={{padding: "20px"}}>
+        <div style={{padding: "30px"}}>
           <div style={containerStyle}>
-            <div>
+            <div style={{ flex: 1 }}>
               <h1>Image Gallery</h1>
             </div>
             <div>
-              <h1>Selected: {selected} </h1>
+              <h1 style={{ flex: 1, textAlign: 'right' }}> {selected} : files selected </h1>
             </div>
           </div>
           <hr></hr>
-          <div >
+          <div className="images-container">
             {Object.values(images).map((image, index) => (
-              <div>
-                <input type="checkbox" onChange={handleCheckboxChange} ></input>
-                <img key={index} src={image} alt={`Image ${index + 1}`} />
+              <div className="image-container" style={{ backgroundImage: `url(${image})` }}>
+                <input class="checkbox" type="checkbox" onChange={handleCheckboxChange}></input>
               </div>
             ))}
+            <div className="image-upload-container" >
+              <input type="file" accept="image/*"  />
+            </div>
           </div>
         </div>
     </>
